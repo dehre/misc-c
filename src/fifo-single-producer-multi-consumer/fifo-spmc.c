3590e580871fd65d65b6029dce62e68539a9b5dd
dehre <loris4526@gmail.com>
2022-08-11 15:26:15 +0200

    single-prod multi-cons fifo queue for mac and linux

---
+++ src/fifo-single-producer-multi-consumer/fifo-spmc.c
@@ -0,0 +1,152 @@
+#include "common.h"
+#include "common_threads.h"
+#include <stdatomic.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define CONSUMER_THREADS_COUNT 3
+
+//==================================================================================================
+// FIFO QUEUE
+//==================================================================================================
+
+#define FIFO_SIZE 3
+int FIFO[FIFO_SIZE];
+int *PutPtr;
+int *GetPtr;
+
+pthread_mutex_t FIFOMutex;
+sem_t *FIFOCurrentSize;
+/* In MacOS the fn `sem_getvalue` is deprecated, so we have to keep track of the semaphore's value on our own */
+atomic_size_t FIFOCurrentSize_val;
+
+typedef enum [[nodiscard]]
+{
+    FIFO_SUCCESS = 0,
+    FIFO_FAIL,
+} FIFO_Rc;
+
+void FIFO_Init(void)
+{
+    PutPtr = GetPtr = &(FIFO[0]);
+    Pthread_mutex_init(&FIFOMutex);
+    Sem_init(FIFOCurrentSize, 0);
+    FIFOCurrentSize_val = 0;
+}
+
+FIFO_Rc FIFO_Put(int data)
+{
+    if (FIFOCurrentSize_val == FIFO_SIZE)
+    {
+        return FIFO_FAIL;
+    }
+
+    *PutPtr = data;
+    PutPtr++;
+    if (PutPtr == &(FIFO[FIFO_SIZE]))
+    {
+        PutPtr = &(FIFO[0]);
+    }
+
+    FIFOCurrentSize_val++;
+    Sem_post(FIFOCurrentSize);
+    return FIFO_SUCCESS;
+}
+
+int FIFO_Get(void)
+{
+    Sem_wait(FIFOCurrentSize);
+    FIFOCurrentSize_val--;
+    Pthread_mutex_lock(&FIFOMutex);
+
+    int data = *GetPtr;
+    GetPtr++;
+    if (GetPtr == &(FIFO[FIFO_SIZE]))
+    {
+        GetPtr = &(FIFO[0]);
+    }
+
+    Pthread_mutex_unlock(&FIFOMutex);
+    return data;
+}
+
+void FIFO_Destroy(void)
+{
+    Pthread_mutex_destroy(&FIFOMutex);
+    Sem_destroy(FIFOCurrentSize);
+}
+
+//==================================================================================================
+// PRODUCER AND CONSUMER THREADS
+//==================================================================================================
+
+/*
+ * Thread `consumer` is passed its argument by value.
+ * It's a bit wild, but does the job.
+ */
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpointer-to-int-cast"
+#elif __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wvoid-pointer-to-int-cast"
+#endif
+
+void *producer(void *arg)
+{
+    (void)arg;
+    for (size_t i = 0; i < 15; i++)
+    {
+        int data = rand() % 100;
+        FIFO_Rc rc = FIFO_Put(data);
+        rc != FIFO_SUCCESS ? printf("Producer: DATA LOST %d\n", data) : printf("Producer: put %d\n", data);
+    }
+    return NULL;
+}
+
+void *consumer(void *arg)
+{
+    int consumer_id = (int)arg;
+    while (1)
+    {
+        int data = FIFO_Get();
+        printf("Consumer %d: get %d\n", consumer_id, data);
+    }
+    return NULL;
+}
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#elif __clang__
+#pragma clang diagnostic pop
+#endif
+
+//==================================================================================================
+// MAIN
+//==================================================================================================
+
+int main(void)
+{
+    if (getuid() != 0)
+    {
+        /* On MacOS `Sem_init` creates a named semaphore, which needs root privileges to be deleted */
+        fprintf(stderr, "please run the program as root\n");
+        exit(1);
+    }
+
+    FIFO_Init();
+    Sranddev();
+    pthread_t producer_thread;
+    pthread_t consumer_threads[CONSUMER_THREADS_COUNT];
+    Pthread_create(&producer_thread, NULL, producer, NULL);
+    for (size_t i = 0; i < CONSUMER_THREADS_COUNT; i++)
+    {
+        Pthread_create(consumer_threads + i, NULL, consumer, (void *)i);
+    }
+
+    Pthread_join(producer_thread, NULL);
+    sleep(1); /* Ideally consumers would run indefinitely */
+    FIFO_Destroy();
+    return 0;
+}
